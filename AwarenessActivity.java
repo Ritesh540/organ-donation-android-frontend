ccd08cddf4ff5a2f50256420a558acdf8f48c370
Ritesh540 <riteshsalunke7890@gmail.com>
2025-12-27 01:45:41 +0530

    Create AwarenessActivity.java

---
+++ AwarenessActivity.java
@@ -0,0 +1,12 @@
+package com.example.organdonation.common;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.organdonation.R;
+
+public class AwarenessActivity extends AppCompatActivity {
+    protected void onCreate(Bundle b) {
+        super.onCreate(b);
+        setContentView(R.layout.activity_awareness);
+    }
+}
